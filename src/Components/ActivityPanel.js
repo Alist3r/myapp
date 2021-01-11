5ba513f06fd283fe0cda39c1c1494126e8b28c12
stefano.quaglietti <stefano.quaglietti@PC-QUAGLIETTI>
2021-01-11 11:37:20 +0100

    First version of Effect per Click activity

--- src/Components/ActivityPanel.js
+++ src/Components/ActivityPanel.js
@@ -10,21 +10,30 @@ class ActivityPanel extends React.Component {
   }
 
   doActivity() {
-    var activityToDo = this.state.activity
-    var resourcesList = this.state.gameResources.slice()
-    if(activityToDo.upgradeCost != null) {
-      var onClickEffect = activityToDo.effectPerClick.slice()
-      onClickEffect.map(effect => (
-        
-      ))
-    }
+    var activityToDo = this.state.activity //one activity
+    var resourcesList = this.state.gameResources.slice() //entire resource list
 
+    // ---------- CLICK EFFECT ---------- //
+    if(activityToDo.effectPerClick != null) { 
+      let effects = activityToDo.effectPerClick.slice()
+      effects.forEach(effect => { //load effects list
+        for(let i=0; i<resourcesList.length;i++) { //apply effect to matching resource
+          if(resourcesList[i].name === effect.resource) {
+            resourcesList[i].currentValue += effect.clickValue //add effect clickvalue
+            this.setState({
+              gameResources: resourcesList
+            })
+          }
+        }
+      });
+
+    }
   }
 
   render() {
     let activity = this.state.activity
       return(
-          <button onClick={() => this.doActivity} className='activity'> {activity.name} | lv: {activity.stage}</button>           
+          <button onClick={() => this.doActivity()} className='activity'> {activity.name} | lv: {activity.stage}</button>           
       )
     }
 }
--- src/Utilities/ActivityList.js
+++ src/Utilities/ActivityList.js
@@ -1,10 +1,12 @@
 const activityList = [
     {
         name: 'Punch the Wall', 
+        description: 'asdasdaaasda',
         stage: 0,
         upgradeCost: null,
         effectPerClick: [
-            { resource: 'Athleticism', clickValue: 1 }
+            { resource: 'Athleticism', clickValue: 1 },
+            { resource: 'Psiche', clickValue: 2}
         ],
         effectPerTick: null,
         defaultUnlocked: true,
