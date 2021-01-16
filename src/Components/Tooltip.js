bec9ae4403f7edc6b95867f6ddc77e5b1dfff420
Alist3r <quagliettistefano@gmail.com>
2021-01-16 16:20:00 +0100

    .

--- src/Components/Tooltip.js
+++ src/Components/Tooltip.js
@@ -29,7 +29,7 @@ const Tooltip = (props) => {
         <div className={`Tooltip-Tip ${props.direction || "top"}`}>
           <div>
             <div className="Tooltip-Description">{props.description}</div>
-            <div className="Divider"></div>
+            <div className="Tooltip-Divider"></div>
             <div className="Tooltip-EffectDesc">{props.effectDesc}</div>
           </div>
         </div>
